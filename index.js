23f87b5bf7949c5e9d37ed9482a83c90397e7d7d
Achilleas Dev <99798880+axilleasdev@users.noreply.github.com>
2024-02-24 22:41:29 +0200

    Update index.js
    new features to js file

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-
-
-if(document.getElementById('conversionMode').value == "lowercase") {
-    document.getElementById("labelFirstLetter").style.display = "block";
-
-} else {
-    document.getElementById("labelFirstLetter").style.display = "none";
-}
-
-
-
 function convertText() {
-  let input = document.getElementById("inputText").value;
-  let mode = document.getElementById("conversionMode").value;
-  let output;
-  if (mode === "lowercase") {
-    if (keepFirstLetterCapitalized.checked == true) {
-      output = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+    let input = document.getElementById("inputText").value;
+    let mode = document.getElementById("conversionMode").value;
+    let output;
 
-    } else {
-      output = input.toLowerCase();
+
+    if (mode === "lowercase") {
+
+        output = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+
+    } else if (mode === "capitalized") {
+        output = removeStress(input).toUpperCase();
+
+    } else if (mode === "titled") {
+        output = convertToTitleCase(input);
+
+    } else if (mode === "toSentenceCase") {
+        output = toSentenceCase(input);
+
     }
-  } else if (mode === "capitalized") {
-    output = removeStress(input).toUpperCase();
 
-  } else if (mode === "titled") {
-    output =  convertToTitleCase(input);
- 
-} else if (mode === "toSentenceCase") {
-  output =  toSentenceCase(input);
 
+    function toSentenceCase(input) {
+        return input.toLowerCase().replace(/(^|\.\s+)\S/g, function(c) { return c.toUpperCase(); });
+    }
+
+
+    function removeStress(input) {
+        return input.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    }
+
+    function convertToTitleCase(input) {
+        let words = input.split(" ");
+        let titleCase = "";
+        for (let word of words) {
+            titleCase += word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() + " ";
+        }
+        return titleCase.trim();
+    }
+
+    let titleCase = convertToTitleCase(input);
+
+    document.getElementById("output").innerHTML = output;
 }
 
-
-function toSentenceCase(input) {
-  return input.toLowerCase().replace(/(^|\.\s+)\S/g, function(c) { return c.toUpperCase(); });
+function copyText() {
+    let text = document.getElementById("output").innerHTML;
+    navigator.clipboard.writeText(text);
 }
 
+document.getElementById("btn-copy").addEventListener("click", function() {
+    copyText();
+});
 
-function removeStress(input) {
-  return input.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-}
 
-function convertToTitleCase(input) {
-  let words = input.split(" ");
-  let titleCase = "";
-  for (let word of words) {
-  titleCase += word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() + " ";
-  }
-  return titleCase.trim();
-}
-
-let titleCase = convertToTitleCase(input);
-
-  document.getElementById("output").innerHTML = output;
-}
+document.getElementById('inputText').addEventListener("input", function() {
+    convertText();
+});
